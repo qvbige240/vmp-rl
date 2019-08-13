7833c97cc862961c20225fbbfa1db79fc36778df
zouqing <qing.zou@timanetworks.com>
2019-08-13 16:14:16 +0800

    modified config

--- execute.sh
+++ execute.sh
@@ -11,6 +11,7 @@ automake --add-missing
 # for protoc
 #export PKG_CONFIG_PATH=/home/zouqing/osource/network/pc/final_protobuf/lib/pkgconfig
 #../configure --prefix=/home/zouqing/work/carnet/linux/auto/vmp/vmp-rl/install --enable-zlog
+#export PATH=/home/zouqing/osource/network/pc/final_protobuf/bin:$PATH
 
 
 if false; then
